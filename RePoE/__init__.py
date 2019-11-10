22c7b9020ba581ab515b446a4b4b4c890b668f44
Nick Ryder <nick.ryder@openai.com>
2019-11-10 14:17:49 -0800

    working on refactor...

--- RePoE/__init__.py
+++ RePoE/__init__.py
@@ -1,66 +1,29 @@
-import argparse
+import json
 
-from RePoE.base_items import write_base_items
-from RePoE.characters import write_characters
-from RePoE.crafting_bench_options import write_crafting_bench_options
-from RePoE.default_monster_stats import write_default_monster_stats
-from RePoE.essences import write_essences
-from RePoE.gem_tags import write_gem_tags
-from RePoE.gems import write_gems
-from RePoE.item_classes import write_item_classes
-from RePoE.mods import write_mods
-from RePoE.stat_translations import write_stat_translations
-from RePoE.stats import write_stats
-from RePoE.tags import write_tags
-from RePoE.fossils import write_fossils
-from RePoE.mod_types import write_types
-from RePoE.util import load_ggpk, create_relational_reader, create_translation_file_cache, \
-    DEFAULT_GGPK_PATH, create_ot_file_cache
+import os
+
+#RePoe.modules
+modules = ['stat_translations', 'mods', 'stats', 'gems', 'gem_tags', 'crafting_bench_options', 'base_items', 'tags', 'item_classes', 'essences', 'default_monster_stats', 'characters', 'fossils', 'types']
 
 
-def main(data_path='../data/'):
-    modules = {
-        'all': None,
-        'stat_translations': write_stat_translations,
-        'mods': write_mods,
-        'stats': write_stats,
-        'gems': write_gems,  # gems.json and gem_tooltips.json
-        'gem_tags': write_gem_tags,
-        'crafting_bench_options': write_crafting_bench_options,
-        'base_items': write_base_items,
-        'tags': write_tags,
-        'item_classes': write_item_classes,
-        'essences': write_essences,
-        'default_monster_stats': write_default_monster_stats,
-        'characters': write_characters,
-        'fossils': write_fossils,
-        'types': write_types,
-        # todo 'buffs': BuffDefinitions.dat?
-    }
+def _get_file_path_from_data_dir(file_name):
+    this_dir, _ = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir, "data", file_name)
+    return DATA_PATH
 
-    parser = argparse.ArgumentParser(description="Convert GGPK files to Json using PyPoE")
-    parser.add_argument('modules', metavar="module", nargs='+', choices=modules.keys(),
-                        help="the converter modules to run (choose from '" + "', '".join(modules.keys()) + "')")
-    parser.add_argument('-f', '--file', default=DEFAULT_GGPK_PATH,
-                        help="path to your Content.ggpk file")
-    args = parser.parse_args()
+def _generate_json_return_func(json_name):
+    '''
+    generates a python function which loads and returns the json located in data/
+    '''
+    def foo():
+        file_path = _get_file_path_from_data_dir(f"{json_name}.json")
+        with open(file_path) as json_data:
+            return json.load(json_data)
+    return foo
 
-    print("Loading GGPK ...", end='', flush=True)
-    ggpk = load_ggpk(args.file)
-    print(" Done!")
-
-    selected_modules = args.modules
-    if 'all' in selected_modules:
-        selected_modules = [m for m in modules if m != 'all']
-
-    rr = create_relational_reader(ggpk)
-    tfc = create_translation_file_cache(ggpk)
-    otfc = create_ot_file_cache(ggpk)
-    for module in selected_modules:
-        print("Running module '%s'" % module)
-        modules[module](ggpk=ggpk, data_path=data_path, relational_reader=rr,
-                        translation_file_cache=tfc, ot_file_cache=otfc)
-
-
-if __name__ == '__main__':
-    main()
+# adds functions for each module which when called return the json file
+# example use: RePoE.mods() returns a python dict which contains the contents of data/mods.json
+# we use a function call here instead of storing the dict to force the end user to 
+# reload the json file incase it has been updated.
+for module_string in modules:
+    locals()[module_string] = _generate_json_return_func(module_string)
