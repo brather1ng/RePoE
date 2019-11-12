dbe8241a64eaed43b068d714bb4e71f68d96ce7a
Nick Ryder <nick.ryder@berkeley.edu>
2019-11-11 20:32:55 -0800

    working more on auto detecting modules

--- RePoE/parser/modules/__init__.py
+++ RePoE/parser/modules/__init__.py
@@ -3,6 +3,7 @@ from os.path import dirname, basename, isfile, join
 from RePoE.parser import Parser_Module
 import glob
 import inspect
+import importlib
 
 def _get_child_classes(module, parent_class):
     child_classes = []
@@ -12,9 +13,11 @@ def _get_child_classes(module, parent_class):
     return child_classes
 
 def get_all_parser_modules():
-    modules = glob.glob(join(dirname(__file__), "*.py"))
-    return [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+    file_names = glob.glob(join(dirname(__file__), "*.py"))
+    module_strings = [ basename(f)[:-3] for f in file_names if isfile(f) and not f.endswith('__init__.py')]
+    return [importlib.import_module(f"RePoE.parser.modules.{module_string}") for module_string in module_strings]
 
-print(get_all_parser_modules())
 for module in get_all_parser_modules():
+    
     print(_get_child_classes(module, Parser_Module))
+
