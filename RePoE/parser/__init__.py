f32550f9612a0c33fd6d267da9b9d2ac78703395
Nick Ryder <nick.ryder@openai.com>
2019-11-11 20:40:23 -0800

    added in parent class

--- RePoE/parser/__init__.py
+++ RePoE/parser/__init__.py
@@ -8,17 +8,3 @@ class Parser_Module():
         ''' method which writes json files to data_path'''
         raise NotImplementedError
 
-
-
-def _get_child_classes(module, parent_class):
-    child_classes = []
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj) and obj.__bases__[0] == parent_class:
-            child_classes.append(obj)
-    return child_classes
-
-def get_all_parser_modules():
-    from os.path import dirname, basename, isfile, join
-    import glob
-    modules = glob.glob(join(dirname(__file__), "/modules/*.py"))
-    __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
--- RePoE/parser/modules/characters.py
+++ RePoE/parser/modules/characters.py
@@ -1,28 +1,31 @@
 from RePoE.parser.util import write_json, call_with_default_args
+from RePoE.parser import Parser_Module
 
-
-def write(data_path, relational_reader, **kwargs):
-    root = []
-    for row in relational_reader['Characters.dat']:
-        root.append({
-            'metadata_id': row['Id'],
-            'integer_id': row['IntegerId'],
-            'name': row['Name'],
-            'base_stats': {
-                'life': row['BaseMaxLife'],
-                'mana': row['BaseMaxMana'],
-                'strength': row['BaseStrength'],
-                'dexterity': row['BaseDexterity'],
-                'intelligence': row['BaseIntelligence'],
-                'unarmed': {
-                    'attack_time': row['WeaponSpeed'],
-                    'min_physical_damage': row['MinDamage'],
-                    'max_physical_damage': row['MaxDamage'],
-                    'range': row['MaxAttackDistance'],
+class characters(Parser_Module):
+    
+    @classmethod
+    def write(data_path, relational_reader, **kwargs):
+        root = []
+        for row in relational_reader['Characters.dat']:
+            root.append({
+                'metadata_id': row['Id'],
+                'integer_id': row['IntegerId'],
+                'name': row['Name'],
+                'base_stats': {
+                    'life': row['BaseMaxLife'],
+                    'mana': row['BaseMaxMana'],
+                    'strength': row['BaseStrength'],
+                    'dexterity': row['BaseDexterity'],
+                    'intelligence': row['BaseIntelligence'],
+                    'unarmed': {
+                        'attack_time': row['WeaponSpeed'],
+                        'min_physical_damage': row['MinDamage'],
+                        'max_physical_damage': row['MaxDamage'],
+                        'range': row['MaxAttackDistance'],
+                    },
                 },
-            },
-        })
-    write_json(root, data_path, 'characters')
+            })
+        write_json(root, data_path, 'characters')
 
 
 if __name__ == '__main__':
