bc96c0a42d060d307c0bcc2f09cf7dc8ccb7776e
brather1ng <7596346+brather1ng@users.noreply.github.com>
2020-03-03 21:54:05 +0100

    Restore quest_rewards data (fix #21)
    GGG changed the way QuestRewards references Quest in 3.9.0.
    Requires a PyPoE state with the changes from OmegaK2/PyPoE#116

--- RePoE/parser/modules/quest_rewards.py
+++ RePoE/parser/modules/quest_rewards.py
@@ -1,47 +1,45 @@
-from RePoE.parser.util import call_with_default_args, write_json, get_release_state, ignore_mod_domain
+from RePoE.parser.util import call_with_default_args, write_json
 from RePoE.parser import Parser_Module
+
 
 class quest_rewards(Parser_Module):
     @staticmethod
     def write(ggpk, data_path, relational_reader, translation_file_cache, ot_file_cache):
         root = {}
-        all_classes = [ "Duelist", "Marauder", "Ranger", "Scion", "Shadow", "Templar", "Witch" ]
+        all_classes = ["Duelist", "Marauder", "Ranger", "Scion", "Shadow", "Templar", "Witch"]
         for reward_row in relational_reader['QuestRewards.dat']:
             if reward_row["BaseItemTypesKey"] is None:
                 continue
-            questId = reward_row["QuestKey"]["Id"]
-            rewardId = reward_row["BaseItemTypesKey"]["Id"]
 
-            if reward_row["BaseItemTypesKey"] is not None:
-                rewardName = reward_row["BaseItemTypesKey"]["Name"].replace("'", "")
-                if reward_row["CharactersKey"] is not None:
-                    charClass = reward_row["CharactersKey"]["Name"]
-                else:
-                    charClass = "All"
-                
+            quest = reward_row["QuestRewardOffersKey"]["QuestKey"]
+            quest_id = quest["Id"]
+            reward_id = reward_row["BaseItemTypesKey"]["Id"]
 
-                # Add an entry for the quest into the root node
-                if questId not in root:
-                    root[questId] = {
-                        "name": reward_row["QuestKey"]["Name"],
-                        "act": reward_row["QuestKey"]["Act"],
-                        "rewards": {}
-                    }
-                
-                ##### This is for root->quest->rewards->class
-                # Add an entry for the reward into the quest node
-                if rewardName not in root[questId]["rewards"]:
-                    root[questId]["rewards"][rewardId] = {
-                        "classes": [],
-                        "name": rewardName,
-                        "type": reward_row["BaseItemTypesKey"]["ItemClassesKey"]["Id"]
-                    }
-                if charClass == "All":
-                    root[questId]["rewards"][rewardId]["classes"] = all_classes
-                else:
-                    if charClass not in root[questId]["rewards"][rewardId]["classes"]:
-                        root[questId]["rewards"][rewardId]["classes"].append(charClass)
+            if quest_id not in root:
+                root[quest_id] = {
+                    "name": quest["Name"],
+                    "act": quest["Act"],
+                    "rewards": {}
+                }
+            rewards = root[quest_id]["rewards"]
+
+            if reward_id not in rewards:
+                rewards[reward_id] = {
+                    "classes": [],
+                    "name": reward_row["BaseItemTypesKey"]["Name"],
+                    "type": reward_row["BaseItemTypesKey"]["ItemClassesKey"]["Id"]
+                }
+            reward = rewards[reward_id]
+
+            if reward_row["CharactersKey"] is None:
+                reward["classes"] = all_classes
+            else:
+                char_class = reward_row["CharactersKey"]["Name"]
+                if char_class not in reward["classes"]:
+                    reward["classes"].append(char_class)
+
         write_json(root, data_path, 'quest_rewards')
+
 
 if __name__ == '__main__':
     call_with_default_args(quest_rewards.write)
