8ea9f439003ee8de66660d3d44752582d221deec
Nick Ryder <nickryder@openai.com>
2021-03-27 16:26:56 -0700

    added in a dict with all stat_translations

--- RePoE/stat_translations.py
+++ RePoE/stat_translations.py
@@ -4,48 +4,39 @@ import os
 
 __STAT_TRANSLATION_PATH__ = os.path.join(__DATA_PATH__, "stat_translations", "")
 
-strongbox = load_json("strongbox.json", __STAT_TRANSLATION_PATH__)
-support_gem = load_json("support_gem.json", __STAT_TRANSLATION_PATH__)
-skill = load_json("skill.json", __STAT_TRANSLATION_PATH__)
-aura_skill = load_json("aura_skill.json", __STAT_TRANSLATION_PATH__)
-banner_aura_skill = load_json("banner_aura_skill.json", __STAT_TRANSLATION_PATH__)
-beam_skill = load_json("beam_skill.json", __STAT_TRANSLATION_PATH__)
-brand_skill = load_json("brand_skill.json", __STAT_TRANSLATION_PATH__)
-buff_skill = load_json("buff_skill.json", __STAT_TRANSLATION_PATH__)
-curse_skill = load_json("curse_skill.json", __STAT_TRANSLATION_PATH__)
-debuff_skill = load_json("debuff_skill.json", __STAT_TRANSLATION_PATH__)
-minion_skill = load_json("minion_skill.json", __STAT_TRANSLATION_PATH__)
-minion_attack_skill = load_json("minion_attack_skill.json", __STAT_TRANSLATION_PATH__)
-minion_spell_skill = load_json("minion_spell_skill.json", __STAT_TRANSLATION_PATH__)
-offering_skill = load_json("offering_skill.json", __STAT_TRANSLATION_PATH__)
-variable_duration_skill = load_json("variable_duration_skill.json", __STAT_TRANSLATION_PATH__)
-areas = load_json("areas.json", __STAT_TRANSLATION_PATH__)
-atlas = load_json("atlas.json", __STAT_TRANSLATION_PATH__)
-passive_skill = load_json("passive_skill.json", __STAT_TRANSLATION_PATH__)
-passive_skill_aura = load_json("passive_skill_aura.json", __STAT_TRANSLATION_PATH__)
-monster = load_json("monster.json", __STAT_TRANSLATION_PATH__)
 
-all = [
-    strongbox,
-    support_gem,
-    skill,
-    aura_skill,
-    banner_aura_skill,
-    beam_skill,
-    brand_skill,
-    buff_skill,
-    curse_skill,
-    debuff_skill,
-    minion_skill,
-    minion_attack_skill,
-    minion_spell_skill,
-    offering_skill,
-    variable_duration_skill,
-    areas,
-    atlas,
-    passive_skill,
-    passive_skill_aura,
-    monster,
-]
+stat_translations = {}
+
+
+def _load_json_and_add_to_stat_translations(path, dir):
+    name, _, _ = path.partition(".json")
+    loaded_json = load_json(path, dir)
+    stat_translations[name] = loaded_json
+    return loaded_json
+
+
+strongbox = _load_json_and_add_to_stat_translations("strongbox.json", __STAT_TRANSLATION_PATH__)
+support_gem = _load_json_and_add_to_stat_translations("support_gem.json", __STAT_TRANSLATION_PATH__)
+skill = _load_json_and_add_to_stat_translations("skill.json", __STAT_TRANSLATION_PATH__)
+aura_skill = _load_json_and_add_to_stat_translations("aura_skill.json", __STAT_TRANSLATION_PATH__)
+banner_aura_skill = _load_json_and_add_to_stat_translations("banner_aura_skill.json", __STAT_TRANSLATION_PATH__)
+beam_skill = _load_json_and_add_to_stat_translations("beam_skill.json", __STAT_TRANSLATION_PATH__)
+brand_skill = _load_json_and_add_to_stat_translations("brand_skill.json", __STAT_TRANSLATION_PATH__)
+buff_skill = _load_json_and_add_to_stat_translations("buff_skill.json", __STAT_TRANSLATION_PATH__)
+curse_skill = _load_json_and_add_to_stat_translations("curse_skill.json", __STAT_TRANSLATION_PATH__)
+debuff_skill = _load_json_and_add_to_stat_translations("debuff_skill.json", __STAT_TRANSLATION_PATH__)
+minion_skill = _load_json_and_add_to_stat_translations("minion_skill.json", __STAT_TRANSLATION_PATH__)
+minion_attack_skill = _load_json_and_add_to_stat_translations("minion_attack_skill.json", __STAT_TRANSLATION_PATH__)
+minion_spell_skill = _load_json_and_add_to_stat_translations("minion_spell_skill.json", __STAT_TRANSLATION_PATH__)
+offering_skill = _load_json_and_add_to_stat_translations("offering_skill.json", __STAT_TRANSLATION_PATH__)
+variable_duration_skill = _load_json_and_add_to_stat_translations(
+    "variable_duration_skill.json", __STAT_TRANSLATION_PATH__
+)
+areas = _load_json_and_add_to_stat_translations("areas.json", __STAT_TRANSLATION_PATH__)
+atlas = _load_json_and_add_to_stat_translations("atlas.json", __STAT_TRANSLATION_PATH__)
+passive_skill = _load_json_and_add_to_stat_translations("passive_skill.json", __STAT_TRANSLATION_PATH__)
+passive_skill_aura = _load_json_and_add_to_stat_translations("passive_skill_aura.json", __STAT_TRANSLATION_PATH__)
+monster = _load_json_and_add_to_stat_translations("monster.json", __STAT_TRANSLATION_PATH__)
+
 
 _assert_all_json_files_accounted_for(__STAT_TRANSLATION_PATH__)
